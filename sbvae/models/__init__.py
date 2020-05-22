5ad26529b28962c94c071ce7f84f0c2ddc26dbaa
Pierre Boyeau <pierreboyeau@s130.Millennium.Berkeley.EDU>
2020-05-22 11:31:02 -0700

    update experiments

--- sbvae/models/__init__.py
+++ sbvae/models/__init__.py
@@ -1,12 +1,11 @@
 from .classifier import Classifier
-from .gaussian_fixed import LinearGaussian, LinearGaussianDefensive
+from .gaussian_fixed import LinearGaussianDefensive
 from .semi_supervised_vae import SemiSupervisedVAE
 from .vae import VAE
 
 __all__ = [
     "VAE",
     "SemiSupervisedVAE",
-    "LinearGaussian",
     "LinearGaussianDefensive",
     "Classifier",
 ]
